77510b8b92e94a897c32f4c9bb6119a0ef85d527
lichenyi <lichenyiwin@outlook.com>
2023-03-05 23:00:16 +0800

    修改日志输出内容格式，添加websocket和接口测试

--- src/main/java/cn/lichenyi/springbootwebsocket/controller/UserController.java
+++ src/main/java/cn/lichenyi/springbootwebsocket/controller/UserController.java
@@ -1,18 +1,21 @@
 package cn.lichenyi.springbootwebsocket.controller;
 
+import cn.lichenyi.springbootwebsocket.websocket.WebsocketServer;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class UserController {
-    static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        logger.info(name);
-        return String.format("Hello %s!", name);
+    @GetMapping("/getContent")
+    public String hello() throws Exception {
+        String result = WebsocketServer.readFile();
+        log.info(result);
+        return result;
     }
 }
--- src/main/java/cn/lichenyi/springbootwebsocket/websocket/WebsocketServer.java
+++ src/main/java/cn/lichenyi/springbootwebsocket/websocket/WebsocketServer.java
@@ -7,35 +7,71 @@ import jakarta.websocket.server.ServerEndpoint;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Component
 @ServerEndpoint(("/ws/{uid}"))
 public class WebsocketServer {
+    static String str = null;
+    static {
+        try {
+            str = readFile();
+        } catch (Exception e) {
+            log.error("读取文件出错");
+            throw new RuntimeException(e);
+        }
+
+    }
+
     @OnOpen
-    public void onOpen(Session session, @PathParam("uid") String uid) throws IOException {
+    public void onOpen(Session session, @PathParam("uid") String uid) throws Exception {
         log.info(uid + " 链接成功");
-        //连接成功
-        session.getBasicRemote().sendText("你好，欢迎【" + uid + "】链接websocket");
+        session.getBasicRemote().sendText(str);
     }
 
     @OnClose
     public void onClose() {
         log.info("链接关闭");
-        System.out.println(this + "链接关闭");
+        System.out.println("connection is closed");
     }
 
     @OnMessage
-    public void onMessage(String message, Session session, @PathParam("uid") String uid) throws IOException {
-        session.getBasicRemote().sendText(uid + " 发送的消息：" + message);
+    public void onMessage(String message, Session session, @PathParam("uid") String uid) throws Exception {
         log.info(uid + " 发送的消息：" + message);
+        session.getBasicRemote().sendText("canGetContent");
     }
 
     @OnError
     public void onError(Session session, Throwable throwable) {
-        System.out.println("发生错误");
+        System.out.println("is error");
         log.error("发生错误");
         throwable.printStackTrace();
+    }
+
+    public static String readFile() throws Exception {
+        StringBuilder  stringBuilder = new StringBuilder();
+        FileInputStream inputStream = new FileInputStream(new File("D:\\download\\QQ\\1097704842\\FileRecv\\2.txt"));
+        FileChannel fileChannel = inputStream.getChannel();
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        // 以下代码也几乎是Buffer和Channle的标准读写操作。
+        while (true) {
+            buffer.clear();
+            int result = fileChannel.read(buffer);
+            buffer.flip();
+            if (result == -1) {
+                break;
+            }
+            //log.info(new String(buffer.array(), 0, result, "UTF-8"));
+            //log.info("---");
+            stringBuilder.append(new String(buffer.array(), 0, result, "UTF-8"));
+        }
+        inputStream.close();
+        return stringBuilder.toString();
     }
 }
